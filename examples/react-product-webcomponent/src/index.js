b52f36fc984d44c9680ce7ea073225cae1f82085
Eugene Tulika <vranen@gmail.com>
2017-09-13 17:02:46 -0500

    Add React example

---
+++ examples/react-product-webcomponent/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MagentoProduct from './MagentoProduct.js';
+
+ReactDOM.render(
+  <MagentoProduct />,
+  document.getElementById('maincontent')
+);
--- examples/react-product-webcomponent/webpack.config.js
+++ examples/react-product-webcomponent/webpack.config.js
@@ -1,11 +1,27 @@
 var path = require('path');
 var webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpackConfig = {
-    entry: './app.js',
+    entry: './src/index.js',
     output: {
-        filename: 'js/output.bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            query:
+            {
+              presets:['react']
+            }
+          },
+          {test: /\.css$/, loader: 'css-loader'},
+          {test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },    
+        ],
+      }
 }
 
 module.exports = webpackConfig;
