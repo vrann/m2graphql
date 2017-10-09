97edb5b65dffe7e07eb2a9fca81ecf71b551e722
Eugene Tulika <vranen@gmail.com>
2017-10-09 21:23:50 +0300

    Move Application logic to the FrontController

--- examples/react-product-webcomponent/src/components/App/index.js
+++ examples/react-product-webcomponent/src/components/App/index.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-
+import ApolloFactory from '../../ApolloFactory'
+import gql from 'graphql-tag';
 import MagentoProductList from '../MagentoProductList';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.queries = [];
+        this.callbacks = [];
+    }
+
+    registerGQL(gqlSnippet) {
+        this.queries.push(gqlSnippet.query)
+        this.callbacks.push(gqlSnippet.callback)
+    }
+
   render() {
     return (
       <div className="App">
-        <MagentoProductList registerGQL={this.props.registerGQL} />
-        <MagentoProductList registerGQL={this.props.registerGQL} />
+        <MagentoProductList registerGQL={this.registerGQL.bind(this)} />
+        <MagentoProductList registerGQL={this.registerGQL.bind(this)} />
       </div>
     );
   }
 
   componentDidMount() {
-      this.props.mounted();
+    const queriesConcat =  '{ ' + this.queries.join(',') + ' }';
+    const query = {
+      query:  gql(queriesConcat)
+    }
+    
+    const client = ApolloFactory();
+    const promise = client.query(query);
+  
+    const callbackPromises = [];
+    this.callbacks.map(callback => {
+      callbackPromises.push(callback(promise))
+    })
+    this.callbacks = []
+    this.queries = []
+  
+    Promise.all(
+      callbackPromises
+    ).then(results => {
+      var toLoad = false;
+      results.map(result => {
+        if (result !== true) {
+          this.registerGQL(result)
+          toLoad = true;
+        }
+      })
+      if (toLoad) {
+        this.componentDidMount()
+      }
+    }).catch(console.log)
   }
 }
 
--- examples/react-product-webcomponent/src/index.js
+++ examples/react-product-webcomponent/src/index.js
@@ -1,53 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import ApolloFactory from './ApolloFactory'
-import gql from 'graphql-tag';
-
-
-var queries = [];
-var callbacks = [];
-
-function registerGQL(gqlSnippet) {
-  queries.push(gqlSnippet.query)
-  callbacks.push(gqlSnippet.callback)
-}
-
-function mounted() {
-  const queriesConcat =  '{ ' + queries.join(',') + ' }';
-  const query = {
-    query:  gql(queriesConcat)
-  }
-  
-  const client = ApolloFactory();
-  const promise = client.query(query);
-
-  const callbackPromises = [];
-  callbacks.map(callback => {
-    callbackPromises.push(callback(promise))
-  })
-  callbacks = []
-  queries = []
-
-  Promise.all(
-    callbackPromises
-  ).then(results => {
-    var toLoad = false;
-    results.map(result => {
-      if (result !== true) {
-        registerGQL(result)
-        toLoad = true;
-      }
-    })
-    if (toLoad) {
-      mounted()
-    }
-  }).catch(console.log)
-}
 
 ReactDOM.render(
   <div>
-    <App registerGQL={registerGQL} mounted={mounted} />
+    <App />
   </div>,
   document.getElementById('maincontent')
 );
